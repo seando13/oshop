d4b8c22e87eff360f1e590197a67af2043b52109
Sean McCusker <sean.mccusker@gmail.com>
2019-05-14 11:54:52 +1000

    Can extract a service

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,6 +1,7 @@
 import { Subscription } from 'rxjs/Subscription';
 import { ProductService } from './../../product.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-admin-products',
@@ -8,7 +9,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
-  products: { title: string }[];
+  products: Product[];
   filteredProducts: any[];
   subscription: Subscription;
 
