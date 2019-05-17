e6bc1166253aeef4ecd25e77ca915ffe11a66ef4
Sean McCusker <sean.mccusker@gmail.com>
2019-05-17 14:37:40 +1000

    Can sort and some pagination

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -2,6 +2,7 @@ import { Subscription } from 'rxjs/Subscription';
 import { ProductService } from './../../product.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { DataTableResource } from '@ismatjon/angular-data-table';
 
 @Component({
   selector: 'app-admin-products',
@@ -9,16 +10,34 @@ import { Product } from 'src/app/models/product';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
-  products: Product[];
+  products: Product[] = [];
   filteredProducts: any[];
   subscription: Subscription;
+  tableResource: DataTableResource<Product>;
+  items: Product[] = [];
+  itemCount: number = 0;
 
   constructor(private productService: ProductService) {
-    this.subscription = this.productService
-      .getAll()
-      .subscribe(
-        products => (this.filteredProducts = this.products = products)
-      );
+    this.subscription = this.productService.getAll().subscribe(products => {
+      this.filteredProducts = this.products = products;
+      this.initializeTable(products);
+    });
+  }
+
+  private initializeTable(products: Product[]) {
+    this.tableResource = new DataTableResource(products);
+    this.tableResource
+      .query({ offset: 0, limit: 10 })
+      .then(items => (this.items = items));
+    this.tableResource.count().then(count => (this.itemCount = count));
+  }
+
+  reloadItems(params) {
+    if (!this.tableResource) {
+      return;
+    }
+
+    this.tableResource.query(params).then(items => (this.items = items));
   }
 
   filter(query: string) {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { CustomFormsModule } from 'ng2-validation';
+import { DataTableModule } from '@ismatjon/angular-data-table';
 
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
@@ -46,6 +47,7 @@ import { ProductService } from './product.service';
   imports: [
     BrowserModule,
     FormsModule,
+    DataTableModule,
     CustomFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
